8dc21578448a981b42ed58a103e865efee682cd1
Anto <vinishanto@gmail.com>
2022-05-31 14:03:27 +0530

    branch test

--- packages/app/components/testSass/index.jsx
+++ packages/app/components/testSass/index.jsx
@@ -2,6 +2,6 @@ import test from './test.module.scss'
 
 export default function TestSass() {
     return <div className={test.testsass}>
-        Hello Sass new
+        Hello Sass new test
     </div>
 }
